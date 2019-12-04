289917c39ade101df31dddd79959ad73dec67b87
Thomas <tjp.hopstaken@ad-academie.nl>
2019-12-04 12:34:12 +0100

    Fixed SQL statement

--- route/get/index.php
+++ route/get/index.php
@@ -1,27 +1,27 @@
 <?php
-	require_once 'Database.php';
-  
+	require_once '../../Database.php';
+
 	$database = new Database();
 	$db = $database->getConnection();
-  
+
 	if (!isset($_POST['boebotID'])) {
 		echo 'Error, no boebotID set';
 		return;
 	}
-  
+
 	$boebotID = $_POST['boebotID'];
-  
-	$stmt = $db->prepare('SELECT * FROM Route WHERE boebotID = :boebotID && status = :status');
-	$stmt->bindParam(':boebotID', $boebotID);
-	$stmt->bindParam(':status', 'start');
+
+	$stmt = $db->prepare('SELECT * FROM route WHERE boebotID = :boebotID AND status = :status');
+	$stmt->bindValue(':boebotID', $boebotID);
+	$stmt->bindValue(':status', 'START');
 	$stmt->execute();
-  
+
 	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  
+
 	if($result){
 		echo json_encode($result);
 	}else{
 		echo "{'route' : 'null'}";
 	}
-  
+
 ?>
