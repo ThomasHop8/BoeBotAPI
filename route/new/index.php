7c9f83fcede3e3767e9c65f6243bab06209afdc7
Thomas <ThomasHop8@users.noreply.github.com>
2019-12-04 12:00:35 +0100

    Update index.php

--- route/new/index.php
+++ route/new/index.php
@@ -1,3 +1,22 @@
 <?php
+	require_once 'Database.php';
 
+	$database = new Database();
+	$db = $database->getConnection();
+	$postValues = array('routeID', 'route', 'boebotID', 'status');
+
+	foreach ($postValues as $postValue) {
+		if (!isset($_POST[$postValue])) {
+			echo "Error, no " . $postValue . ' value set';
+			return;
+		}
+	}
+
+	$stmt = $db->prepare('INSERT INTO Route VALUES(:routeID, :route, :boebotID, :status)');
+	$stmt->bindParam(':routeID', $_POST['routeID']);
+	$stmt->bindParam(':route', $_POST['route']);
+	$stmt->bindParam(':boebotID', $_POST['boebotID']);
+	$stmt->bindParam(':status', $_POST['status']);
+
+	echo json_encode(array('succes' => $stmt->execute()));
 ?>
